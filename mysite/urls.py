290d865505fe4f51a93500de3e9f4c8560499bb6
pallavi0902 <pallavichauhan@unomaha.edu>
2018-01-29 23:07:02 -0600

    Django First Blog, first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/',
-        include('blog.urls',
-        namespace='blog',
-        app_name='blog')),
+    url(r'^blog/', include('blog.urls',
+                           namespace='blog',
+                           app_name='blog')),
 ]
